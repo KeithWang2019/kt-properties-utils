c907c63138157f58865786bca6be5b0661c0997f
藏书君 <gg_happy@163.com>
2025-11-16 12:15:24 +0800

    init

--- src/test/java/com/cangsg/brick/kt/entry/KtTest17.java
+++
@@ -1,9 +0,0 @@
-package com.cangsg.brick.kt.entry;
-
-import lombok.Data;
-
-@Data
-public class KtTest17<T> {
-    private T t1;
-    private KtTestEnum e1;
-}
--- src/test/java/com/cangsg/brick/kt/entry/KtTest18.java
+++
@@ -1,9 +0,0 @@
-package com.cangsg.brick.kt.entry;
-
-import lombok.Data;
-
-@Data
-public class KtTest18<T> {
-    private T t1;
-    private KtTestEnum2 e1;
-}
